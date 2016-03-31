ad93e77873c15967764df7c647e52dfd967847d4
Debbie <detsenak@ualberta.ca>
2016-03-31 08:59:07 -0600

    the hidden fields are now enabled when the admin adds a new day

--- resources/assets/js/add_day.js
+++ resources/assets/js/add_day.js
@@ -1,21 +1,8 @@
 function addDay(){
-    var days= getElementById("days").value
-    var newnumDays = <?php echo $numDays; ?>;
-    <?php echo $numDays; ?> = days;
-    // var day = $(".new-day").last().clone();
-    // day.removeClass("hidden");
-    // //student.find("input").prop('disabled', false);
-    // day.appendTo("#newDay");
+  var day = $(".new-day").last().clone();
+  day.removeClass("hidden");
+  day.find("input").prop('disabled', false);
+  var days = {{$numDays}};
+  //{{$numDays}} = {{$numDays}} + 1;
+  day.appendTo("#all-days");
 }
-
-$(document).ready(function(){
-    $("#group-checkbox").click(function(){
-        var checkbox = $("#group-checkbox");
-        if($("#group-checkbox").is(":checked")){
-            addDay();
-        }else{
-            $("#new-days").empty();
-        }
-    });
-
-});
--- resources/views/user/admin/dayTimes.blade.php
+++ resources/views/user/admin/dayTimes.blade.php
@@ -1,33 +1,19 @@
 <div class="form-group{{ $errors->has('days') ? ' has-error' : '' }}">
   <label class="col-md-3 control-label" id="days">Number of days</label>
 
-  <div class="col-md-6">
-    <select type="text" class="form-control" name="days">
-      <option value=""> Specify the number of days for this conference </option>
-      <option value="" disabled >---------</option>
-      @foreach($days as $day)
-        <option value="{{ $day }}"
-           {{old('day') == $day ? 'selected' : ''}}>
-        {{ $day }}</option>
-      @endforeach
-    </select>
-
-  </div>
-  <div class="col-md-1 checkbox">
+  <div class="col-md-1 button">
     <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1 text-center">
-        <a class="btn btn-default" href="#" onclick="addDay();">
-            <i class="fa fa-plus"></i>
+        <a class="btn btn-default" onclick="addDay(); enable();">
+            Add a Day
         </a>
     </div>
   </div>
 </div>
 
 
-
-<div id=oneday>
-  @for($i= 1; $i <= $numDays; $i++)
+<div id=all-days>
+  @for($i= 1; $i <= $_SESSION['numDays']; $i++)
   <h3><b> Day {{$i}} </b></h3>
-  <div id=dayTime>
   <div class="form-group{{ $errors->has('start_time') ? ' has-error' : '' }}">
     <label class="col-md-3 control-label">Start time</label>
 
@@ -58,20 +44,20 @@
           {{ $time }}</option>
         @endforeach
       </select>
-
     </div>
   </div>
   @endfor
-  </div>
 </div>
 
 
 <div class="hidden form-group new-day">
+
+  <h3 id='num'><b> Day {{$_SESSION['numDays']=$_SESSION['numDays'] + 1}} </b></h3>
   <div class="form-group{{ $errors->has('start_time') ? ' has-error' : '' }}">
     <label class="col-md-3 control-label">Start time</label>
 
     <div class="col-md-6">
-      <select type="text" class="form-control" name="start_time[]">
+      <select type="text" class="form-control" name="start_time[]" id="startTime" disabled>
         <option value=""> earliest time for the conference </option>
         <option value="" disabled >---------</option>
         @foreach($times as $time)
@@ -88,7 +74,7 @@
     <label class="col-md-3 control-label"> Start time of latest presentation</label>
 
     <div class="col-md-6">
-      <select type="text" class="form-control" name="end_time[]">
+      <select type="text" onchange="enable()" class="form-control" name="end_time[]" id="endTime" disabled>
         <option value=""> Latest time for the conference </option>
         <option value="" disabled >---------</option>
         @foreach($times as $time)
@@ -103,5 +89,18 @@
 </div>
 
 @section('scripts')
-<script src="{{ asset('js/add_day.js') }}"></script>
+<script>
+function enable() {
+    document.getElementById("endTime").disabled=false;
+    document.getElementById("startTime").disabled=false;
+}
+function addDay(){
+  var day = $(".new-day").last().clone();
+  day.removeClass("hidden");
+  //document.getElementById("num").innerHTML=day1;
+  day.find("input").prop('disabled', false);
+  //{{$_SESSION['numDays'] = $_SESSION['numDays'] + 1}};
+  day.appendTo("#all-days");
+}
+</script>
 @stop
