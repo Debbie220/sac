4ad173663c1c1ae44ca36df3a378f488a1dc58f2
Debbie <detsenak@ualberta.ca>
2016-04-02 15:12:17 -0600

    moved script to js file

--- resources/views/user/admin/dayTimes.blade.php
+++ resources/views/user/admin/dayTimes.blade.php
@@ -88,26 +88,5 @@
 </div>
 
 @section('scripts')
-<script>
-
-var numdays = 1;
-
-function enable() {
-    document.getElementById("endTime").disabled=false;
-    document.getElementById("startTime").disabled=false;
-}
-
-function addDay(){
-  numdays = numdays + 1;
-  var day = $(".new-day").last().clone();
-  var header = $(".header").last().clone();
-  header.removeClass("hidden");
-  header.find("b").text("Day "  + numdays);
-  day.removeClass("hidden");
-  enable();
-  header.appendTo("#all-days");
-  day.appendTo("#all-days");
-
-}
-</script>
+<script src="{{ asset('js/add_day.js') }}"></script>
 @stop
