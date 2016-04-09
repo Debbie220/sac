03051a77df60811d5c779f3510216fe7c0074ea6
Parley Martins <parleypachecomartins@gmail.com>
2016-04-08 21:36:35 -0600

    Add sticky footer

--- resources/views/basePages/uofafooter.blade.php
+++ resources/views/basePages/uofafooter.blade.php
@@ -1,7 +1,8 @@
 <br>
-
-<div class="uofa container-fluid footer">
-  <p class="navbar-text ">
-    <span class="uofatext">2016</span>
-  </p>
-</div>
+<footer class="footer uofa">
+    <div class="container">
+      <p class="navbar-text ">
+        <span class="uofatext">2016</span>
+      </p>
+    </div>
+</footer>
