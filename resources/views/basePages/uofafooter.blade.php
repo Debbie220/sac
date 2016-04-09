a16302328499eac5a4024d8dd8427f6088a80705
Parley Martins <parleypachecomartins@gmail.com>
2016-04-09 12:56:58 -0600

    Add help and make login and signup bigger

--- resources/views/basePages/uofafooter.blade.php
+++ resources/views/basePages/uofafooter.blade.php
@@ -1,8 +1,13 @@
 <br>
 <footer class="footer uofa">
     <div class="container">
-      <p class="navbar-text ">
+      <p class="navbar-text">
         <span class="uofatext">2016</span>
+      </p>
+      <p class="navbar-text">
+        <span class="uofatext">
+        	<a class="uofatext" href="https://aucsc.github.io/sac">Help</a>
+        </span>
       </p>
     </div>
 </footer>
--- resources/views/static/home.blade.php
+++ resources/views/static/home.blade.php
@@ -1,7 +1,12 @@
 @extends('basePages.sac')
 
 @section('content')
-	<div class="jumbotron">
-		<h1>SAC Home Page</h1>
-	</div>
+    <div class="jumbotron">
+        <h1> Welcome to the SAC registration system!</h1>
+        <h2>
+            <a  href="{{ url('/register') }}" class="btn btn-primary btn-lg"> Signup Now!</a> or
+            <a  href="{{ url('/login') }}" class="btn btn-default btn-lg"> Login</a>
+        </h2>
+
+    </div>
 @endsection
