61e71e9913ee11dca3e96e10035ff19c2213ec7e
Parley Martins <parleypachecomartins@gmail.com>
2016-04-11 22:57:25 -0600

    Select old conference implemented

--- app/Http/Controllers/ConferencesController.php
+++ app/Http/Controllers/ConferencesController.php
@@ -6,12 +6,22 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Conference;
+use App\Presentation;
 
 class ConferencesController extends Controller
 {
-    function old(){
+    function old(Request $request) {
+    	$id = $request['id'];
+    	if($id == null)
+    		$id = get_current_conference_id();
     	$conferences = Conference::where('id', '<>', get_current_conference_id())->
     		orderBy('id', 'desc')->get();
-    	return view('conferences.old', compact('conferences'));
+   		$chosen_conf = Conference::where('id', '=', $id)->first();
+    	$presentations = Presentation::where('conference_id', '=', $id)->
+    		where('status', '=', 'A')->
+    		orderBy('course_id')->
+    		get();
+    	return view('conferences.old',
+    		compact('conferences', 'chosen_conf', 'presentations'));
     }
 }
--- resources/views/conferences/old.blade.php
+++ resources/views/conferences/old.blade.php
@@ -5,11 +5,86 @@ Old Conferences
 @stop
 
 @section('admin_content')
-	@forelse($conferences as $conference)
-		<p>
-			{{$conference->description}}
-		</p>
-	@empty
-		No previous conferences
-	@endforelse
+
+    <form action="{{ route('conference.old') }}" method="GET">
+        <div class="col-lg-11 col-md-11 col-sm-11 col-xs-9 text-center">
+            <select type="text" class="form-control" name="id" >
+                <option value=""> Select a conference </option>
+                <option value="" disabled >---------</option>
+
+                @foreach($conferences as $conference)
+                    <option value="{{ $conference->id }}">
+                    {{ $conference->description }}</option>
+                @endforeach
+            </select>
+        </div>
+
+        <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1 text-center">
+            <button type="submit" class="btn btn-default" id="add_course">
+                Select Conference
+            </button>
+        </div>
+    </form>
+
+    <br><br>
+        <h3><b> {{ $chosen_conf->description }} </b> selected.</h3>
+    <hr>
+
+    @forelse($presentations as $p)
+        <div class="row ">
+            <div class="col-lg-8 col-md-8 col-sm-8 info">
+                <h4>
+                    {{$p['title']}}
+                </h4>
+                <div>
+                    <p>
+                        <b>Professor:</b> {{ $p['professor_name'] }}
+                    </p>
+                    <p>
+                        <b>Course:</b> {{$p->course->toString()}}
+                    </p>
+                    <p>
+                        <b>Students:</b>
+                        <ul class="list-unstyled">
+                        @foreach($p->students() as $student)
+                            <li>{{ $student->student_name }}</li>
+                        @endforeach
+                        </ul>
+                    </p>
+                    <p>
+                        <b>Abstract:</b> {{$p['abstract']}}
+                    </p>
+
+                </div>
+            </div>
+
+
+            <div class="col-lg-1 col-md-1 col-sm-1 text-center">
+                @if($p['our_nominee'])
+                    <span title="OUR Nominee"><i class="fa fa-star fa-lg"></i></span>
+                @endif
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-2 ">
+                {{$p->course->subject_code . " " . $p->course->number}}
+            </div>
+
+            <div class="col-lg-1 col-md-1 col-sm-1 ">
+                <p class = "
+                    @if($p->status == 'S')
+                        label label-warning
+                    @elseif($p->status == 'D')
+                        label label-danger
+                    @elseif($p->status == 'A')
+                        label label-success
+                    @else
+                        label label-info
+                    @endif">
+                    {{ $p->status()->get()->first()->description }}
+                </p>
+            </div>
+        </div>
+        <hr>
+    @empty
+        <h4>No approved presentations found for this conference or no conference selected!</h4>
+    @endforelse
 @stop
