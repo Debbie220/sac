2cfd8196de51a31046d7c0a52077af588fb3b594
Debbie <detsenak@ualberta.ca>
2016-04-12 16:52:21 -0600

    no changes made

--- resources/views/user/professor/my_courses.blade.php
+++ resources/views/user/professor/my_courses.blade.php
@@ -20,8 +20,8 @@
                 <th class="col-lg-9 col-md-9 col-sm-9 text-center">Title</th>
                 <th class="col-lg-1 col-md-1 col-sm-1 text-center"></th>
             </tr>
-            
-            
+
+
             @foreach($my_courses as $course)
             <tr class="row">
                 <td class="text-center">
