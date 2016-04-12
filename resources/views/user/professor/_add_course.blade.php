7f5c24ecc7fcc060543c3aca2ca2c4bcbfd01a4d
Debbie <detsenak@ualberta.ca>
2016-04-12 16:50:15 -0600

    changed submission button to say confirm selection

--- resources/views/user/professor/_add_course.blade.php
+++ resources/views/user/professor/_add_course.blade.php
@@ -1,6 +1,6 @@
 <form action="{{ route('add_course') }}" method="POST">
 	{{ csrf_field() }}
-	<div class="col-lg-11 col-md-11 col-sm-11 col-xs-9 text-center">
+	<div class="col-lg-1 col-md-10 col-sm-1 col-xs-1 text center">
 		<select type="text" class="form-control" name="course_id" id="course">
 			<option value=""> Select a course </option>
 			<option value="" disabled >---------</option>
@@ -14,7 +14,8 @@
 
 	<div class="col-lg-1 col-md-1 col-sm-1 col-xs-1 text-center">
 		<button type="submit" class="btn btn-default" id="add_course">
-			<i class="fa fa-plus"></i> Add Course
+			<i class="fa fa-plus"></i> Confirm selection
 		</button>
 	</div>
+
 </form>
