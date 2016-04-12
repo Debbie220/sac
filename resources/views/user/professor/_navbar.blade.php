547c249d32e60bb1a91ad3c7a9494a4fd33bdc61
Debbie <detsenak@ualberta.ca>
2016-04-12 16:51:50 -0600

    added new button to redirect to home page and changed my courses to edit courses

--- resources/views/user/professor/_navbar.blade.php
+++ resources/views/user/professor/_navbar.blade.php
@@ -1,8 +1,14 @@
 <p class="navbar-text">
-  <a href="{{ route('my_courses') }}">
+  <a style="margin-right: 1em;" href="{{ route('home') }}">
     <span class="uofatext">
       <i class="fa fa-list"></i>
-      My Courses
+      Presentations
+    </span>
+  </a>
+  <a href="{{ route('my_courses') }}">
+    <span class="uofatext">
+      <i class="fa fa-pencil-square-o"></i>
+      Edit Courses
     </span>
   </a>
 </p>
