6e14f5bede1deb8d69c414062e0b145c82f4d13c
Debbie <detsenak@ualberta.ca>
2016-04-27 09:17:49 -0600

    can now preview presentations according to days

--- resources/views/timeslots/preview.blade.php
+++ resources/views/timeslots/preview.blade.php
@@ -7,72 +7,31 @@
 @section('admin_content')
 @foreach($rooms as $room)
   <div class="row ">
-    <div class="col-lg-6 col-md-6 col-sm-6 ">
+    <div class="col-lg-12 col-md-12 col-sm-12">
         <h2>
             <a href="#">
                 {{$room['code']}}
             </a>
         </h2>
 
-        <div class="row">
-            <table class="table">
-                <tr class="row">
-                    <th class="col-lg-1 col-md-1 col-sm-1 text-center">Time</th>
-                    <th class="col-lg-1 col-md-1 col-sm-1 text-center">Name</th>
-                    <th class="col-lg-7 col-md-7 col-sm-7 text-center">Presentation Title</th>
-                    <th class="col-lg-1 col-md-1 col-sm-1 text-center">Course</th>
-                    <th class="col-lg-1 col-md-1 col-sm-1 text-center">Professor Name</th>
-                    <th class="col-lg-1 col-md-1 col-sm-1 text-center">Type</th>
-                </tr>
-
-                @foreach($presentations as $p)
-                @if($p['timeslot'] != null)
-                @if($p->timeslot()->get()->first()->room_code == {{room['code']}} )
-
-                <tr class="row">
-                    <td class="text-center">
-                        <p>
-                            {{ $p->timeslot()->get()->first()->time}}
-                        </p>
-                    </td>
-                    <td class="text-center">
-                        <p>
-                            {{ $p->owner()->get()->first()->name}}
-                        </p>
-                    </td>
-                    <td class="text-center">
-                        <p>
-                            {{ $p['title']}}
-                        </p>
-                    </td>
-                    <td class="text-center">
-                        <p>
-                            {{ $p->course()->get()->first()->subject_code}}
-                        </p>
-                    </td>
-                    <td class="text-center">
-                        <p>
-                            {{ $p['professor_name']}}
-                        </p>
-                    </td>
-                    <td class="text-center">
-                        <p>
-                            {{ $p->type()->get()->first()->description}}
-                        </p>
-                    </td>
-                </tr>
-                @endif
-                @endif
-                @endforeach
-            </table>
+        @for($day = 1; $day <= $days; $day++)
+          <h3> Day {{$day}} </h3>
+          <div class="row">
+              <table class="table">
+                  <tr class="row">
+                      <th class="col-lg-1 col-md-1 col-sm-1 text-center">Time</th>
+                      <th class="col-lg-2 col-md-2 col-sm-2 text-center">Name</th>
+                      <th class="col-lg-4 col-md-4 col-sm-4 text-center">Presentation Title</th>
+                      <th class="col-lg-2 col-md-2 col-sm-2 text-center">Course</th>
+                      <th class="col-lg-2 col-md-2 col-sm-2 text-center">Professor Name</th>
+                      <th class="col-lg-1 col-md-1 col-sm-1 text-center">Type</th>
+                  </tr>
+                  @include('timeslots._preview_table_body')
+          </table>
         </div>
-
-
+      @endfor
     </div>
   </div>
 
-@endforeach
-@foreach($timeslots as $timeslot)
-{{$timeslot}}
 @endforeach
 @stop
